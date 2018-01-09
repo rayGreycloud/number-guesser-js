ca34a4683db10d9ae01a55d05dfbd6db372c5fc8
rayGreycloud <rayGreycloud@gmail.com>
2018-01-09 09:15:59 -0800

    Add block comment outlining game functionality

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+GAME FUNCTION:
+- Player guesses number btwn min and max 
+- Player gets certain number of guesses  
+- Display number of guesses remaining 
+- Notify player of win if guess correct 
+- Notify player of loss if no guesses remaining
+- Display correct answer if player loses
+- Play again button 
+*/
+
